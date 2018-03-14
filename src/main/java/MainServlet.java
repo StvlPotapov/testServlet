9f0ef1da5fc063791cbad7cd760bb6ee359b21df
Stepan Potapov <stvlpotapov@gmail.com>
2018-03-14 11:54:53 +0400

    add mysql and hibernate dependencies

--- src/main/java/MainServlet.java
+++ src/main/java/MainServlet.java
@@ -3,11 +3,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class MainServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        //super.doGet(req, resp);
+        PrintWriter out = resp.getWriter();
+        out.print("<h1>hello world </h1>");
     }
 
     @Override
