cb7b155a26c37ce6c526821e2929bef67e724bd5
Stepan Potapov <stvlpotapov@gmail.com>
2018-03-16 01:34:29 +0400

    repositories

--- src/main/java/repositories/UserPaymentMethodRepository.java
+++ src/main/java/repositories/UserPaymentMethodRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserPaymentMethodRepository extends CrudRepository<UserPaymentMethod, Long> {
 
+
+
 }
--- src/main/java/repositories/UserRepository.java
+++ src/main/java/repositories/UserRepository.java
@@ -11,4 +11,6 @@ public interface UserRepository extends CrudRepository<User,Long> {
 
     List<User> findUserByLastName(String lastName); //попытка воспользоваться ключевыми словами Spring
 
+
+
 }
