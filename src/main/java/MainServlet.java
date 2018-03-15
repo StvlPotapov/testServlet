e51820cdfa07c21d550578d80cd5f25631ccd7ce
Stepan Potapov <stvlpotapov@gmail.com>
2018-03-15 23:25:59 +0400

    add testing str

--- src/main/java/MainServlet.java
+++ src/main/java/MainServlet.java
@@ -1,3 +1,5 @@
+import repositories.UserRepository;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -6,11 +8,20 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class MainServlet extends HttpServlet {
+
+    UserRepository rep;  //тестовая строка
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //super.doGet(req, resp);
+
         PrintWriter out = resp.getWriter();
         out.print("<h1>hello world </h1>");
+        out.print(rep.findUserByLastName("sdfghj"));  //еще одна тестовая строка
+        out.print(req.getPathInfo());
+        out.print(req.getMethod());
+        out.print(req.getQueryString());
+
+
     }
 
     @Override
