f2b973a4b1f14612a31d1643b5e9216345480d8b
Stepan Potapov <stvlpotapov@gmail.com>
2018-03-16 15:12:19 +0400

    trying

--- src/main/java/MainServlet.java
+++ src/main/java/MainServlet.java
@@ -1,3 +1,8 @@
+import entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import repositories.UserRepository;
 
 import javax.servlet.ServletException;
@@ -7,22 +12,43 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@RestController
 public class MainServlet extends HttpServlet {
 
-    UserRepository rep;  //тестовая строка
+    @Autowired
+    UserRepository repository;  //тестовая строка
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        repository.delete((Long) req.getAttribute("id"));
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //super.doGet(req, resp);
 
-        PrintWriter out = resp.getWriter();
+        /*PrintWriter out = resp.getWriter();
         out.print("<h1>hello world </h1>");
-        out.print(rep.findUserByLastName("sdfghj"));  //еще одна тестовая строка
-        out.print(req.getPathInfo());
-        out.print(req.getMethod());
+        out.print(rep.findUserByLastName("sdfghj"));  //еще одна тестовая строка */
+        System.out.print(req.getPathInfo());
+        System.out.print(req.getMethod());
         out.print(req.getQueryString());
 
 
+
+        repository.findAll();
+
     }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPut(req, resp);
+
+    }
+
+
+
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
